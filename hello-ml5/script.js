104d8f97e6927fabdce2675681644d7866ba479d
Lennart Küppers <lennartkueppers@web.de>
2024-05-06 01:51:55 +0200

    5.0

--- hello-ml5/script.js
+++ hello-ml5/script.js
@@ -6,6 +6,9 @@ const dropArea = document.getElementById("drop-area");
 const loader = document.getElementById("loader")
 const classfiyButton = document.getElementById("classifyButton")
 const chart = document.getElementById("chartArea")
+const exampleChart = document.getElementById("exampleChart")
+const right = document.getElementById("right")
+const wrong = document.getElementById("wrong")
 
 // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
 let classifier;
@@ -28,25 +31,39 @@ function setup() {
 }
 
 images.forEach(function(image) {
+  if(image.id !=wrong.id && image.id !=right.id){
     image.addEventListener('mouseover', function() {
-        images.forEach(function(otherImage) {
-            otherImage.style.opacity = '0.2'; // Setze die Opacity auf 0.2 für andere Bilder
-        });
-        image.style.opacity = '1'; // Setze die Opacity auf 1 für das gehoverten Bild
-        diagrams.forEach(function(otherDiagram) {
-            otherDiagram.style.opacity = '1';
-        });
-        diagrams[0].src=image.src;
-    });
+      images.forEach(function(otherImage) {
+          otherImage.style.opacity = '0.2'; // Setze die Opacity auf 0.2 für andere Bilder
+      });
+      image.style.opacity = '1'; // Setze die Opacity auf 1 für das gehoverten Bild
+      diagrams.forEach(function(otherDiagram) {
+          otherDiagram.style.opacity = '1';
+      });
+      if(image.id == "Image1" || image.id == "Image2" || image.id == "Image3"){
+        right.style.opacity = '1';
+      }else{
+        wrong.style.opacity = '1';
+      }
 
-    image.addEventListener('mouseout', function() {
-        images.forEach(function(otherImage) {
-            otherImage.style.opacity = '1'; // Setze die Opacity zurück
-        });
-        diagrams.forEach(function(otherDiagram) {
-            otherDiagram.style.opacity = '0'; // Setze die Opacity auf 0.2 für andere Bilder
-        });
-    });
+      diagrams[0].src=image.src;
+      if(!initialClassify){
+        classifier.classify(image, gotResultForExampleDiagram);
+      }
+  });
+
+  image.addEventListener('mouseout', function() {
+    if(image.id !=wrong.id && image.id !=right.id){
+      images.forEach(function(otherImage) {
+        otherImage.style.opacity = '1'; // Setze die Opacity zurück
+      });
+      diagrams.forEach(function(otherDiagram) {
+          otherDiagram.style.opacity = '0'; // Setze die Opacity auf 0.2 für andere Bilder
+      });
+      removeExampleDiagram();
+    }
+  });
+  }
 });
 
 dropArea.addEventListener("dragover", function(e){
@@ -58,6 +75,16 @@ dropArea.addEventListener("drop", function(e){
     inputFile.files = e.dataTransfer.files;
     uploadImage();
 });
+
+// classify result
+function gotResultForExampleDiagram(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    showExampleDiagram(results);
+  }
+}
 
 // classify result
 function gotResult(error, results) {
@@ -101,6 +128,7 @@ function enableClassifyButton(){
 let xValues;
 let yValues; 
 let resultChart;
+let exampleResultChart;
 const barColors = [
     "rgba(121, 121, 121,1)",
     "rgba(121, 121, 121,0.6)",
@@ -146,11 +174,56 @@ function showDiagram(results){
         }
     });
 
-      if(chart.style.display="none"){
-        chart.style.display = "block";
-      }else{
-        chart.style.display = "block";
+    if(chart.style.display="none"){
+      chart.style.display = "block";
+    }
+}
+
+function showExampleDiagram(results){
+  if (exampleResultChart) exampleResultChart.destroy();
+
+  xValues = [results[0].label,results[1].label,results[2].label];
+  yValues = [results[0].confidence, results[1].confidence, results[2].confidence]
+
+  exampleResultChart = new Chart("exampleChart", {
+      type: "bar",
+      data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+            display: true,
+            text: "Classification Result"
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+              ticks: {
+                  maxRotation: 0,
+                  minRotation: 0,
+                  callback: function(value, index, values) {
+                    // Split the labels to show them on multiple lines
+                    return value.split(",");
+                  }
+              }
+          }]
       }
+      }
+  });
+
+  if(exampleChart.style.display="none"){
+    exampleChart.style.display = "block";
+  }
+}
+
+function removeExampleDiagram(){
+  if (exampleResultChart) exampleResultChart.destroy();
 }
 
 function removeAllData(){
